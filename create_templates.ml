c10a4e1bdcddddf3f53aaa1c2f1bdffe6d1caef5
Paul Durrant <paul.durrant@citrix.com>
2015-09-07 09:05:27 +0000

    Revert eaaa7cdc "Remove reference TSC enlightenment for now"
    This should have been reverted a long time ago.
    
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- create_templates.ml
+++ create_templates.ml
@@ -51,6 +51,7 @@ let default_viridian_key_value = "true"
 
 let viridian_flag = viridian_key_name, default_viridian_key_value
 let viridian_time_ref_count_flag = ("viridian_time_ref_count","true")
+let viridian_reference_tsc_flag = ("viridian_reference_tsc","true")
 let nx_flag = ("nx","true")
 let no_nx_flag = ("nx","false")
 let base_platform_flags = ["acpi","1";"apic","true";"pae","true";"hpet","true"]
@@ -359,7 +360,7 @@ let hvm_template
 		(make_long_name name architecture is_experimental) in
 	let platform_flags = base_platform_flags
 		@ (if List.mem StdVga flags then ["vga","std";"videoram","8"] else [])
-		@ (if List.mem Viridian flags then [ viridian_flag;viridian_time_ref_count_flag ] else [])
+		@ (if List.mem Viridian flags then [ viridian_flag;viridian_time_ref_count_flag;viridian_reference_tsc_flag ] else [])
 		@ (if device_id <> "" then [ "device_id", device_id ] else []) in
 	{
 		base with
