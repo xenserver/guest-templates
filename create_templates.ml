c5e9e4cb3a0c941703df4e1b9793506636680985
Salvatore Cambria <salvatore.cambria@citrix.com>
2015-12-03 13:03:27 +0000

    CP-14511: add SLES/SLED 12 SP1 templates

--- create_templates.ml
+++ create_templates.ml
@@ -630,6 +630,8 @@ let create_all_templates rpc session_id =
 		sles11_template    "SUSE Linux Enterprise Desktop 11 SP3" X64 [    ];
 		sles12_template    "SUSE Linux Enterprise Server 12"     X64 [    ];
 		sles12_template    "SUSE Linux Enterprise Desktop 12" X64 [    ];
+		sles12_template    "SUSE Linux Enterprise Server 12 SP1" X64 [    ];
+		sles12_template    "SUSE Linux Enterprise Desktop 12 SP1" X64 [    ];
 
  		debian_template "Debian Squeeze 6.0" "squeeze" X32 [    ];
  		debian_template "Debian Squeeze 6.0" "squeeze" X64_debianlike ~max_mem_gib:70 [    ];
