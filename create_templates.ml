9fec0dc2d9680817c56b9d02398ab3a7fef93b34
Salvatore Cambria <salvatore.cambria@citrix.com>
2016-02-24 15:56:12 +0000

    CP-15935: add template for Ubuntu 16.04

--- create_templates.ml
+++ create_templates.ml
@@ -610,6 +610,7 @@ let create_all_templates rpc session_id =
 		hvm_linux_template "Oracle Linux 7" (GiB 1) (GiB 1500)  (GiB 10);
 		hvm_linux_template "Scientific Linux 7" (GiB 1) (GiB 1500)  (GiB 10);
 		hvm_linux_template "Ubuntu Trusty Tahr 14.04" (MiB 512) (GiB 1500)  (GiB 8);
+		hvm_linux_template "Ubuntu Xenial Xerus 16.04" (MiB 512) (GiB 1500)  (GiB 10);
 		hvm_linux_template "CoreOS" (MiB 512) (GiB 512) (GiB 5);
 		hvm_linux_template "Debian Jessie 8.0" (MiB 128) (GiB 1500)  (GiB 8);
 		sles10sp1_template "SUSE Linux Enterprise Server 10 SP1" X32 [    ];
